60ed794102fbe8f1c57d4fa2f84968d608e0ee71
Samuel Smith <smithsc2@hotmail.com>
2024-02-11 14:30:29 -0400

    added variables required for SwedenMap.js

--- src/config/variables.js
+++ src/config/variables.js
@@ -2,3 +2,188 @@
 
 // ArcGIS variables
 export const arcgisStyles = "https://js.arcgis.com/4.28/@arcgis/core/assets/esri/themes/light/main.css";
+
+export const provinceLabels = [
+  'Lappland', 'Norrbotten', 'Västerbotten', 'Ångermanland', 'Jämtland', 'Medelpad', 'Härjedalen', 
+  'Hälsingland', 'Dalarna', 'Gästrikland', 'Uppland', 'Västmanland', 'Värmland', 'Dalsland', 'Närke', 
+  'Södermanland', 'Bohuslän', 'Västergötland',
+   'Östergötland', 'Gotland', 'Halland', 'Småland', 'Öland', 'Blekinge', 'Skåne'
+]
+// A list of Swedens Provincial Flowers and soime information.
+export const provinceFlowers = {
+  'Lappland': { 
+    flowerName: 'Dryas octopetala',
+    info: 'The provincial flower of Lappland is the fjällsippa, typically found growing on dry rocky terrain.',
+    url: 'https://swedesinthestates.com/swedens-provincial-flowers/',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Dryas_octopetala_DSCF1565.JPG/1024px-Dryas_octopetala_DSCF1565.JPG',
+    attr: `By Teun Spaans - I took this photo near Salvan, <a href="https://creativecommons.org/licenses/by/2.5" title="Creative Commons Attribution 2.5">CC BY 2.5</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=2907821">Link</a>`
+   },
+   'Bohuslän': { 
+    flowerName: 'Lonicera periclymenum',
+    info: 'Vildkaprifol (honeysuckle), is quite common throughout Europe.',
+    url: 'https://swedesinthestates.com/swedens-provincial-flowers/',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Lonicera_periclymenum_Luc_Viatour.JPG/1024px-Lonicera_periclymenum_Luc_Viatour.JPG',
+    attr: `By I, <a href="//commons.wikimedia.org/wiki/User:Lviatour" title="User:Lviatour">Luc Viatour</a>, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=1434436">Link</a>`
+   },
+   'Gotland': { 
+    flowerName: 'Hedera helix',
+    info: 'English Ivy, locally known Murgröna often had its leaves and berries used for old medicines.',
+    url: 'https://swedesinthestates.com/swedens-provincial-flowers/',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/4/40/Hedera_helix_clinging.jpg',
+    attr: `By <a href=&quot;//commons.wikimedia.org/wiki/User:Chery&quot; title=&quot;User:Chery&quot;<chery</a> - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;<Own work</span>, Public Domain, <a href="https://commons.wikimedia.org/w/index.php?curid=1282155">Link</a>`
+   },
+   'Uppland': { 
+    flowerName: 'Fritillaria meleagris',
+    info: 'Kungsängslilja blooms in late spring, and its name translates literally as Snakes Head.',
+    url: 'https://swedesinthestates.com/swedens-provincial-flowers/',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Kungs%C3%A4ngslilja%2C_L%C3%B8ten%2C_Norge_maj-04.jpg/1024px-Kungs%C3%A4ngslilja%2C_L%C3%B8ten%2C_Norge_maj-04.jpg',
+    attr: `By User <a href=&quot;https://sv.wikipedia.org/wiki/Anv%C3%A4ndare:Hasse_A&quot; class=&quot;extiw&quot; title=&quot;sv:Användare:Hasse A&quot;>Hasse A</a> on <a class=&quot;external text&quot; href=&quot;https://sv.wikipedia.org&quot;>sv.wikipedia</a> - Photo by Hans Aldhammer, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=1005220">Link</a>`
+   },
+   'Västmanland': { 
+    flowerName: 'Viscum album',
+    info: 'Mistel is has white berries and is often hung at Christmas time, frequently in doorways.',
+    url: 'https://swedesinthestates.com/swedens-provincial-flowers/',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Mistletoe_P1210829.jpg/1024px-Mistletoe_P1210829.jpg',
+    attr: `By <a href=&quot;//commons.wikimedia.org/wiki/User:David.Monniaux&quot; title=&quot;User:David.Monniaux&quot;>David Monniaux</a> - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;>Own work</span>, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=2202630">Link</a>`
+   },
+   'Härjedalen': { 
+    flowerName: 'Pulsatilla vernalis',
+    info: 'Mosippa, also known as the Spring Pasque flower was added to the IUCN Red List of Threatened species in 2014.',
+    url: 'https://www.iucnredlist.org/species/55730086/55730098',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Pulsatilla_vernalis%2C_Seiser_Alm.JPG/125px-Pulsatilla_vernalis%2C_Seiser_Alm.JPG',
+    attr: `By Tinelot Wittermans, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=1624">Link</a>`
+   },
+   'Blekinge': { 
+    flowerName: 'Verbascum thapsus',
+    info: 'Kungsljus, commonly known as Mullein is native to only a few regions but is very adabtable and is considered invasive in many countries.',
+    url: 'https://www.cabidigitallibrary.org/doi/10.1079/cabicompendium.56652',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Starr_040723-0267_Verbascum_thapsus.jpg/800px-Starr_040723-0267_Verbascum_thapsus.jpg',
+    attr: 'By <a rel="nofollow" class="external text" href="http://www.starrenvironmental.com/">Forest &amp; Kim Starr</a>, <a href="https://creativecommons.org/licenses/by/3.0" title="Creative Commons Attribution 3.0">CC BY 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=6129367">Link</a>'
+   },
+   'Dalarna': { 
+    flowerName: 'Campanula patula',
+    info: 'Ängsklocka, or Spreading Bellflower is most commonly seen with purple petals but on rare occasions you may find them in white!',
+    url: 'https://luontoportti.com/en/t/214/spreading-bellflower',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Campanula_patula.jpg/1024px-Campanula_patula.jpg',
+    attr: `By Sergey M. Sazhin – <a href=&quot;//commons.wikimedia.org/w/index.php?title=User:Sazhin64&amp;amp;action=edit&amp;amp;redlink=1&quot; class=&quot;new&quot; title=&quot;User:Sazhin64 (page does not exist)&quot;<Sazhin64</a> – <a rel=&quot;nofollow&quot; class=&quot;external free&quot; href=&quot;http://sazhin64.narod.ru/&quot;<http://sazhin64.narod.ru/</a> - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;<Own work</span>, <a href="http://www.gnu.org/licenses/gpl.html" title="GNU General Public License">GPL</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=643159">Link</a>`
+   },
+   'Västerbotten': { 
+    flowerName: 'Pedicularis sceptrum-carolinum',
+    info: "Kung Karls spira, commonly know as Moor-king lousewort doesn't spread well because it's flowers make it difficult for any buyt the strongest insect to get at the nectar.",
+    url: 'https://luontoportti.com/en/t/1353/moor-king',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Pedicularis_sceptrum-carolinum_A.jpg/800px-Pedicularis_sceptrum-carolinum_A.jpg',
+    attr: `By Thomas Meyer, Günzburg - meyerguenzburg@onlinehome.de, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=2079524">Link</a>`
+   },
+   'Norrbotten': { 
+    flowerName: 'Rubus arcticus',
+    info: "Åkerbär is regarded by many to have the best wild berries in Finland and while it is most common in Finland and Sweden, it can also be found in British Columbia where it's  called Nagoonberry.",
+    url: 'https://swedesinthestates.com/swedens-provincial-flowers/',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Rubus_arcticus2.jpg/1024px-Rubus_arcticus2.jpg',
+    attr: `By <a href=&quot;https://fi.wikipedia.org/wiki/K%C3%A4ytt%C3%A4j%C3%A4:kompak&quot; class=&quot;extiw&quot; title=&quot;fi:Käyttäjä:kompak&quot;>fi:Käyttäjä:kompak</a> - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;>Own work</span>, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=1025142">Link</a>`
+   },
+   'Ångermanland': { 
+    flowerName: 'Viola tricolor',
+    info: 'Styvmorsviol gets its scientific name from the fact that is flower is often three colours: yellow, light violet and dark violet.',
+    url: 'https://luontoportti.com/en/t/1140/wild-pansy',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Viola_tricolor.jpg/1024px-Viola_tricolor.jpg',
+    attr: `By No machine-readable author provided. <a href=&quot;//commons.wikimedia.org/w/index.php?title=User:Tom.k&amp;amp;action=edit&amp;amp;redlink=1&quot; class=&quot;new&quot; title=&quot;User:Tom.k (page does not exist)&quot;>Tom.k</a> assumed (based on copyright claims). - No machine-readable source provided. Own work assumed (based on copyright claims)., <a href="https://creativecommons.org/licenses/by-sa/2.5" title="Creative Commons Attribution-Share Alike 2.5">CC BY-SA 2.5</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=466692">Link</a>`
+   },
+   'Jämtland': { 
+    flowerName: 'Gymnadenia nigra',
+    info: 'Brunkulla is native to European mountain ranges common in the alps, but has come to be an endangered species on account of global warming harming populations of the insects that typically polinate them.',
+    url: 'https://www.sciencedirect.com/science/article/pii/S2351989421001104?via%3Dihub',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Oesterreichisches_Kohlroeschen.jpg/800px-Oesterreichisches_Kohlroeschen.jpg',
+    attr: `<a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=596858">Link</a>`
+   },
+   'Medelpad': { 
+    flowerName: 'Trollius europaeus',
+    info: 'Smörboll typically flower in May, but in more northern tundra regions, can be found flowering as late as August.',
+    url: 'https://luontoportti.com/en/t/965/globeflower',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/1/12/Vessertal_trollblumen.jpg',
+    attr: `By <a href=&quot;//commons.wikimedia.org/wiki/User:SehLax&quot; title=&quot;User:SehLax&quot;<SehLax</a> - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;>Own work</span>;, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=264041">Link</a>`
+   },
+   'Hälsingland': { 
+    flowerName: 'Linum usitatissimum',
+    info: 'Lin is a variety of flax known as Fairy Flax and can do well in both wet and dry habitats.  As Fairy flax gets older it grows more flowers, starting with only one in its first year, and having many branches in its second.',
+    url: 'https://luontoportti.com/en/t/1085/fairy-flax',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/0/07/Vlas_bloem.jpg',
+    attr: `<a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=540629">Link</a>`
+   },
+   'Gästrikland': { 
+    flowerName: 'Convallaria majalis',
+    info: 'Liljekonvalj (Lily of the Valley) is not only the provinical flower of Gästrikland, but also the national flower of Finland.',
+    url: 'https://luontoportti.com/en/t/235/lily-of-the-valley',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/Convallaria_majalis0.jpg',
+    attr: `<a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=5184">Link</a>`
+   },
+   'Värmland': { 
+    flowerName: 'Trientalis europaea',
+    info: 'Skogsstjärna is quite wide spread despite relying primarily on one species of insect that is rarely observed on the flowers.',
+    url: 'https://luontoportti.com/en/t/195/chickweed-wintergreen',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/XN_Trientalis_europaea_503.jpg/1024px-XN_Trientalis_europaea_503.jpg',
+    attr: `By <a href=&quot;//commons.wikimedia.org/wiki/User:XN&quot; title=&quot;User:XN&quot;>Guido Gerding</a> - Personal photograph taken by Author, URL: <a rel=&quot;nofollow&quot; class=&quot;external text&quot; href=&quot;http://www.exnatura.de/de&quot;>Ex&amp;nbsp;:: Natura - Freies Portal für Umweltbildung (Environmental Education)</a>, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=2057358">Link</a>`
+   },
+   'Närke': { 
+    flowerName: 'Primula veris',
+    info: 'Gullviva are considered to be an important sign of spring of blooming ass winter recedes.',
+    url: 'https://luontoportti.com/en/t/52/marsh-marigold',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/CowSlipSelsley.jpg/1024px-CowSlipSelsley.jpg',
+    attr: `Public Domain, <a href="https://commons.wikimedia.org/w/index.php?curid=125275">Link</a>`
+   },
+   'Dalsland': { 
+    flowerName: 'Myosotis scorpioides',
+    info: 'Förgätmigej is one of many varieties of forget-me-not. This variety thrives in wet environments like damp meadows and ditches.',
+    url: 'https://swedesinthestates.com/swedens-provincial-flowers/',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/6/60/Myosotis_palustris_a1.jpg',
+    attr: `By Jerzy Opioła (Poland) - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;<Own work</span>, <a href="https://creativecommons.org/licenses/by/2.5" title="Creative Commons Attribution 2.5">CC BY 2.5</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=770391">Link</a>`
+   },
+   'Södermanland': { 
+    flowerName: 'Nymphaea alba',
+    info: 'The white water lily roots in the soft bottoms of ponds and slow rivers and flowers at the surface. Therre is a red flowered variety that is conseidered a protected species in most of Finland.',
+    url: 'https://luontoportti.com/en/t/110/white-water-lily',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Nymphaea_alba_26-8-2007_15-13-19.JPG/1024px-Nymphaea_alba_26-8-2007_15-13-19.JPG',
+    attr: `By <a href=&quot;//commons.wikimedia.org/wiki/User:Opuntia&quot; title=&quot;User:Opuntia&quot;>Opuntia</a> - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;>Own work</span>, <a href="https://creativecommons.org/licenses/by-sa/3.0" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=2632780">Link</a>`
+   },
+   'Västergötland': { 
+    flowerName: 'Calluna vulgaris',
+    info: 'Ljung is a good example of plants who thrive after a wildfire. They do not do well in shaded environments, so a wildfire will keeo its environment clear, and they recover quickly.',
+    url: 'https://luontoportti.com/en/t/46/heather',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/30/CallunaVulgaris.jpg',
+    attr: `By <a href=&quot;//commons.wikimedia.org/wiki/User:Aqwis&quot; title=&quot;User:Aqwis&quot;>Aqwis</a> (<a href=&quot;https://en.wikipedia.org/wiki/User:Aqwis&quot; class=&quot;extiw&quot; title=&quot;w:User:Aqwis&quot;>Aqwis</a>) - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;>Own work</span>, <a href="https://creativecommons.org/licenses/by-sa/3.0" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=2558741">Link</a>`
+   },
+   'Östergötland': { 
+    flowerName: 'Centaurea cyanus',
+    info: 'Blåklint (Cornflower) Thrived before industrial agriculture, where it fell victim to weedkillers and seed cleaning. However it has been making a comeback in recent years.',
+    url: 'https://luontoportti.com/en/t/373/cornflower',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Detailaufnahme_Weizenfeld.jpg/1024px-Detailaufnahme_Weizenfeld.jpg',
+    attr: `By <a href=&quot;//commons.wikimedia.org/wiki/User:Queryzo&quot; title=&quot;User:Queryzo&quot;>Queryzo</a> - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;>Own work</span>, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=705031">Link</a>`
+   },
+   'Halland': { 
+    flowerName: 'Genista pilosa',
+    info: 'Hårginst is an evergreen native to Sweden, it grows quickly and prefers dry summer heat.',
+    url: 'https://pfaf.org/user/Plant.aspx?LatinName=Genista+pilosa',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/2/2f/Genista_pilosa2.jpg',
+    attr: `By Kurt Stüber <a rel=&quot;nofollow&quot; class=&quot;external autonumber&quot; href=&quot;http://www.kurtstueber.de/&quot;<[1]</a> - <a rel=&quot;nofollow&quot; class=&quot;external text&quot; href=&quot;http://caliban.mpiz-koeln.mpg.de/mavica/index.html&quot;<caliban.mpiz-koeln.mpg.de/mavica/index.html</a> part of <a rel=&quot;nofollow&quot; class=&quot;external text&quot; href=&quot;http://www.biolib.de&quot;<www.biolib.de</a>, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=5674">Link</a>`
+   },
+   'Småland': { 
+    flowerName: 'Linnaea borealis',
+    info: "Linnea (Twin Flower) gets it's scientific name in dedication to Carl von Linné, who developed the current classification system of plants.",
+    url: 'https://luontoportti.com/en/t/418/twin-flower',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/0/07/Linnaea_borealis.jpg',
+    attr: `By Henripekka Kallio - Uploaded by <a href=&quot;//commons.wikimedia.org/wiki/User:Alterego(fin)&quot; title=&quot;User:Alterego(fin)&quot;>Alterego(fin)</a>, <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=250736">Link</a>`
+   },
+   'Öland': { 
+    flowerName: 'Helianthemum oelandicum',
+    info: 'Ölandssolvända only grows in dry, rocky limestone areas. Part of its scientific name comes from the island of Öland where Carl Linneaus found it in 1741.',
+    url: 'https://www.first-nature.com/flowers/helianthemum-oelandicum.php',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Solv%C3%A4nda.jpg/1024px-Solv%C3%A4nda.jpg',
+    attr: `By <a href=&quot;//commons.wikimedia.org/wiki/User:Stiwed&quot; title=&quot;User:Stiwed&quot;>Stiwed</a> - <span class=&quot;int-own-work&quot; lang=&quot;en&quot;>Own work</span>, Public Domain, <a href="https://commons.wikimedia.org/w/index.php?curid=3267908">Link</a>`
+   },
+   'Skåne': { 
+    flowerName: 'Leucanthemum vulgare',
+    info: 'Prästkrage, the oxeye daisy is one of the most common flowers and when in bloom it will stay open for a month and the yellow center will live on even after the white petals have fallen off.',
+    url: 'https://luontoportti.com/en/t/277/oxeye-daisy',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Several_daisies_%28Asteroideae%29.jpg/1024px-Several_daisies_%28Asteroideae%29.jpg',
+    attr: `<a href="http://www.gnu.org/copyleft/fdl.html" title="GNU Free Documentation License">GFDL</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=594058">Link</a>`
+   },
+}

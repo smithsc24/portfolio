774f0a61046832aee7edf170c22c489eb07930d9
Samuel Smith <smithsc2@hotmail.com>
2024-05-10 13:17:14 -0300

    Updated basic navigation
     Moved navigation tot he homepage, added links for subpages to the Maps
     view.
     Will move Navigation to sidebar, this commit is to ensure that I
     remember how ton user react-router-dom. It's been awhile.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter as Router, Route } from "react-router-dom"; //Navigation modules
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Navigation modules
 
 import Header from "./elements/Header";
 import Footer from "./elements/Footer";
-
 import ArcGISMap from "./components/arcgis/ArcGISMap";
 import SwedenMap from "./components/SwedenMap";
 import MapBox from "./components/mapbox/MapBox";
+
+import Maps from "./pages/Maps";
+import HomePage from "./pages/HomePage"
 import { MapboxProvider } from "./providers/MapboxProvider";
 
 //<ArcGISMap />
@@ -13,9 +15,22 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <MapboxProvider>
-        <MapBox />   
-      </MapboxProvider>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/">
+            <Route index={true} element={<HomePage/>}></Route>
+          </Route>
+          <Route path="/Maps">
+            <Route index={true} element={<Maps/>}></Route>
+            <Route path=":arcgis" element={<ArcGISMap/>}></Route>
+            <Route path=":ai2html" element={<SwedenMap/>}></Route>
+            <Route path=":mapbox" element={<MapboxProvider><MapBox/></MapboxProvider>}></Route>
+          </Route>
+        </Routes>
+      </Router>
+    </div>     
+      
       <Footer />
     </div>
   );
--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -1,40 +1,22 @@
 import React from "react";
-import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
+import { Link  } from "react-router-dom";
 
 import ArcGISMap from "../components/arcgis/ArcGISMap";
 import SwedenMap from "../components/SwedenMap";
 import MapBox from "../components/mapbox/MapBox";
 
-import { MapboxProvider } from "./providers/MapboxProvider";
+import { MapboxProvider } from "../providers/MapboxProvider";
 
 
 const Maps = () => {
-  // this will let us build relative urls and paths
-  // Will we? Maybe later, but not for now
-  let { path, url } = useRouteMatch();
   return (
     <div className="map-container">
-      <div className="map-tabs">
-        <ul>
-          <li>
-            <Link to={`${url}/arcgis`}>ArcGIS JS</Link>
-          </li>
-          <li>
-            <Link to={`${url}/ai2html`}>ai2html</Link>
-          </li>
-          <li>
-          <Link to={`${url}/mapbox`}>Mapbox</Link>
-          </li>
-        </ul>
-      </div>
-      <Switch>
-        <Route exact path={path}>
-          <h3>Please select a map to view</h3>
-        </Route>
-        <Route path={`${path}/arcgis`}><ArcGISMap/></Route>
-        <Route path={`${path}/ai2html`}><SwedenMap/></Route>
-        <Route path={`${path}/mapbox`}><MapboxProvider><MapBox/></MapboxProvider></Route>
-      </Switch>
+      <h2>Choose a map</h2>
+      <ul>
+      <Link to="/Maps/arcgis">ArcGIS</Link>
+      <Link to="/Maps/ai2html">ai2html</Link>
+      <Link to="/Maps/mapbox">Mapbox</Link>
+      </ul>
     </div>
   )
 }

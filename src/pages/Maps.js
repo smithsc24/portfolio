ab120c04decb4bf4ac064a4472e216b389898362
Samuel Smith <smithsc2@hotmail.com>
2024-05-20 23:08:09 -0300

    Updated map navbar, added outlet to display maps
    This change also adds styling to the page, bringing it in line with the
    wireframe. Uses the Outlet from react-router-dom to display each webmap
    when their button is selected.

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { Link  } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 import ArcGISMap from "../components/arcgis/ArcGISMap";
 import SwedenMap from "../components/SwedenMap";
-import MapBox from "../components/mapbox/MapBox";
+import MapBox from "../components/mapbox/MapBox"
+import Button from '../elements/Button';
+
 
 import { MapboxProvider } from "../providers/MapboxProvider";
 
+import '../styles/pages/maps.scss'
 
 const Maps = () => {
   return (
     <div className="map-container">
-      <h2>Choose a map</h2>
-      <ul>
-      <Link to="/Maps/arcgis">ArcGIS</Link>
-      <Link to="/Maps/ai2html">ai2html</Link>
-      <Link to="/Maps/mapbox">Mapbox</Link>
-      </ul>
+      <div className="map-navbar">
+        <Button path="/Maps/arcgis" label="ArcGIS"/>
+        <Button path="/Maps/ai2html" label="ai2html"/>
+        <Button path="/Maps/mapbox" label="Mapbox"/>
+      </div>
+      <div className="map-view">
+        <Outlet/>
+      </div>
     </div>
   )
 }

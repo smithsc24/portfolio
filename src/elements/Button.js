3a458baa164e581c02c9ec15b3eceed1be0281f2
Samuel Smith <smithsc2@hotmail.com>
2024-05-20 23:16:32 -0300

    removed console.log

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -4,7 +4,6 @@ import {Link} from "react-router-dom";
 import '../styles/elements/button.scss'
 
 const Button = ({path, label}) => {
-  console.log(path, label)
   return (
     <div className="btn link-btn">
       <Link to={path}>{label}</Link>

f9c0e19700ea105dae99cc5ce1600a52d6c0bcdd
Samuel Smith <smithsc2@hotmail.com>
2024-05-20 23:17:07 -0300

    Moved routes out of Sidebar into App
    There wasn't really any need for them to be in the sidebar. Feels better
    to have them in App, and keep Sidebar limited to the links.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Navigation modules
+import { Routes, Route, Outlet } from 'react-router-dom'
+import ArcGISMap from "./components/arcgis/ArcGISMap";
+import SwedenMap from "./components/SwedenMap";
+import MapBox from "./components/mapbox/MapBox";
+import Maps from "./pages/Maps";
+import HomePage from "./pages/HomePage"
+
+import { MapboxProvider } from "./providers/MapboxProvider";
 
 import Header from "./elements/Header";
 import Footer from "./elements/Footer";
@@ -9,11 +16,21 @@ import Sidebar from "./elements/Sidebar"
 
 //<ArcGISMap />
 function App() {
+  
   return (
     <div className="App">
-      <Header/>
-      <Sidebar/>     
-      <Footer />
+       <Sidebar/>
+       <Outlet/>   
+       <Routes>
+          <Route path="/">
+              <Route index={true} element={<HomePage/>}/>
+          </Route>
+          <Route path="/Maps" element={<Maps/>}>
+            <Route path="arcgis" element={<ArcGISMap/>}/>
+            <Route path="ai2html" element={<SwedenMap/>}/>
+            <Route path="mapbox" element={<MapboxProvider><MapBox/></MapboxProvider>}/>
+          </Route>
+        </Routes>
     </div>
   );
 }
--- src/elements/Sidebar.js
+++ src/elements/Sidebar.js
@@ -1,34 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Navigation modules
 import Button from './Button';
-import ArcGISMap from "../components/arcgis/ArcGISMap";
-import SwedenMap from "../components/SwedenMap";
-import MapBox from "../components/mapbox/MapBox";
-import Maps from "../pages/Maps";
-import HomePage from "../pages/HomePage"
 
-import { MapboxProvider } from "../providers/MapboxProvider";
-
+import '../styles/elements/sidebar.scss'
 const Sidebar = () => {
+
   return (
     <div className="sidebar">
-          <Routes>
-            <Route path="/">
-              <Route index={true} element={<HomePage/>}></Route>
-            </Route>
-            <Route path="/Maps">
-              <Route index={true} element={<Maps/>}></Route>
-              <Route path="arcgis" element={<ArcGISMap/>}></Route>
-              <Route path="ai2html" element={<SwedenMap/>}></Route>
-              <Route path="mapbox" element={<MapboxProvider><MapBox/></MapboxProvider>}></Route>
-            </Route>
-          </Routes>
-        <div className="links">
-          <Button
-            path="/Maps"
-            label="Webmaps"
-          />
-        </div>
+      <div className="links">
+        <Button path="/" label="Home" />
+        <Button path="/Maps" label="Webmaps" />
+      </div>
     </div>
   )
 }

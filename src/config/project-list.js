41e71d99a0cb7af4d25a49cbae158a393e2ac871
Samuel Smith <smithsc2@hotmail.com>
2024-05-22 21:04:33 -0300

    Added a config file to hold project data
    There's is probably a better way to this, but this will suffice for now.
    I should move all of this into redux, and access it that way. File under
    todo

---
+++ src/config/project-list.js
@@ -0,0 +1,30 @@
+export const projectList = [
+  {
+    name: "Rocky Mountain National Park Atlas",
+    description: "This project was my first attempt at creating an atlas. My goal was to showcase the different ecoregions (Montane, Subalpine and Tundra) within the park boundaries",
+    imgDir: '/images/projects/romo/',
+    thumbnail: "thumbnail.png",
+    images: ['TableofContents.jpg', 'References.jpg', 'Plates_A1.jpg', 'Plates_A2.jpg', 'Plates_A3.jpg', 'Plates_B1.jpg', 'Plates_B2.jpg', 'Plates_B3.jpg', 'Plates_C1.jpg', 'Plates_C2.jpg', 'Plates_C3.jpg', 'Plates_D1.jpg', 'Plates_D2.jpg', 'Plates_D3.jpg', 'Plates_E1.jpg', 'Plates_E2.jpg', 'Plates_E3.jpg']
+  },
+  {
+    name: "Test",
+    imgDir: '',
+    thumbnail: '',
+    description: "This is a description of the project",
+    images: "This will be either a list of images, or a reference to the images directory"
+  },
+  {
+    name: "Test",
+    imgDir: '',
+    thumbnail: '',
+    description: "This is a description of the project",
+    images: "This will be either a list of images, or a reference to the images directory"
+  },
+  {
+    name: "Test",
+    imgDir: '',
+    thumbnail: '',
+    description: "This is a description of the project",
+    images: "This will be either a list of images, or a reference to the images directory"
+  },
+]

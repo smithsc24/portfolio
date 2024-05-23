17ff9b2a0a6177b75abaf72513183c61d54da311
Samuel Smith <smithsc2@hotmail.com>
2024-05-23 13:44:59 -0300

    put in some updated information for terrain atlas plates

--- src/config/project-list.js
+++ src/config/project-list.js
@@ -7,8 +7,8 @@ export const projectList = [
     images: ['TableofContents.jpg', 'References.jpg', 'Plates_A1.jpg', 'Plates_A2.jpg', 'Plates_A3.jpg', 'Plates_B1.jpg', 'Plates_B2.jpg', 'Plates_B3.jpg', 'Plates_C1.jpg', 'Plates_C2.jpg', 'Plates_C3.jpg', 'Plates_D1.jpg', 'Plates_D2.jpg', 'Plates_D3.jpg', 'Plates_E1.jpg', 'Plates_E2.jpg', 'Plates_E3.jpg']
   },
   {
-    name: "Test",
-    imgDir: '',
+    name: "Terrain plates",
+    imgDir: '/images/projects/terrain',
     thumbnail: '',
     description: "This is a description of the project",
     images: "This will be either a list of images, or a reference to the images directory"

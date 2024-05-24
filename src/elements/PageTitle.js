558cb744aac8450b74994ddf61ca1d9f804b1f26
Samuel Smith <smithsc2@hotmail.com>
2024-05-23 21:55:49 -0300

    added generic title component, Makes adding a consistent title block really easy

---
+++ src/elements/PageTitle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../styles/elements/title.scss";
+
+const PageTitle = ({title}) => (
+  <div className="title">
+    <h2>{title}</h2>
+  </div>
+)
+
+export default PageTitle

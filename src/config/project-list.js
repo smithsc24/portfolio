165ff46d1ce2c0b6f2579f6001dba3196fd72179
Samuel Smith <smithsc2@hotmail.com>
2024-05-23 22:47:33 -0300

    added more images and data for project page

--- src/config/project-list.js
+++ src/config/project-list.js
@@ -7,18 +7,19 @@ export const projectList = [
     images: ['TableofContents.jpg', 'References.jpg', 'Plates_A1.jpg', 'Plates_A2.jpg', 'Plates_A3.jpg', 'Plates_B1.jpg', 'Plates_B2.jpg', 'Plates_B3.jpg', 'Plates_C1.jpg', 'Plates_C2.jpg', 'Plates_C3.jpg', 'Plates_D1.jpg', 'Plates_D2.jpg', 'Plates_D3.jpg', 'Plates_E1.jpg', 'Plates_E2.jpg', 'Plates_E3.jpg']
   },
   {
-    name: "Terrain plates",
-    imgDir: '/images/projects/terrain',
-    thumbnail: '',
-    description: "This is a description of the project",
-    images: "This will be either a list of images, or a reference to the images directory"
+    name: "Terrain Atlas Plates",
+    imgDir: '/images/projects/terrain/',
+    thumbnail: 'terrain_thumbnail.png',
+    description: "These terrain plates were all created as part of an atlas demonstrating various techniques for enhancing hillshades, landSAT imagery, and even using blender to render an prepared DEM. I selected a few of the images that I liked the most from the project, both the process and the result.",
+    images: ['GrosMorne_DEM_5m.png', 'landSAT.png', 'Lower Burlington Terrain.png', 'MtFuji_Ihmof.png', 'NZ_Rendered_Southland-Otago.png']
+    
   },
   {
-    name: "Test",
-    imgDir: '',
-    thumbnail: '',
-    description: "This is a description of the project",
-    images: "This will be either a list of images, or a reference to the images directory"
+    name: "Camp Nedooae Scouting Map",
+    imgDir: 'images/projects/nedooae/',
+    thumbnail: 'CampNedooae.png',
+    description: "This map was created for a client through NSCC with the goal that it be used by scouts looking to get one of their badges. Data was obtained from the Nova Scotia Topographic Database and processed in ArcGIS Pro. Pro was used to create a basic layout, but all the final edits were completed with Adobe Illustrator.",
+    images: ['CampNedooae.png']
   },
   {
     name: "Test",

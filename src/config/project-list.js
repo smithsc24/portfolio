41e851debc210a20a84ab55e73e8a97ae8d0edd4
Samuel Smith <smithsc2@hotmail.com>
2024-07-01 11:12:00 -0300

    fixed paths for project images

--- src/config/project-list.js
+++ src/config/project-list.js
@@ -2,13 +2,13 @@ export const projectList = [
   {
     name: "Rocky Mountain National Park Atlas",
     description: "This project was my first attempt at creating an atlas. My goal was to showcase the different ecoregions (Montane, Subalpine and Tundra) within the park boundaries",
-    imgDir: '/images/projects/romo/',
+    imgDir: '/portfolio/images/projects/romo/',
     thumbnail: "thumbnail.png",
     images: ['TableofContents.jpg', 'References.jpg', 'Plates_A1.jpg', 'Plates_A2.jpg', 'Plates_A3.jpg', 'Plates_B1.jpg', 'Plates_B2.jpg', 'Plates_B3.jpg', 'Plates_C1.jpg', 'Plates_C2.jpg', 'Plates_C3.jpg', 'Plates_D1.jpg', 'Plates_D2.jpg', 'Plates_D3.jpg', 'Plates_E1.jpg', 'Plates_E2.jpg', 'Plates_E3.jpg']
   },
   {
     name: "Terrain Atlas Plates",
-    imgDir: '/images/projects/terrain/',
+    imgDir: '/portfolio/images/projects/terrain/',
     thumbnail: 'terrain_thumbnail.png',
     description: "These terrain plates were all created as part of an atlas demonstrating various techniques for enhancing hillshades, landSAT imagery, and even using blender to render an prepared DEM. I selected a few of the images that I liked the most from the project, both the process and the result.",
     images: ['GrosMorne_DEM_5m.png', 'landSAT.png', 'Lower Burlington Terrain.png', 'MtFuji_Ihmof.png', 'NZ_Rendered_Southland-Otago.png']
@@ -16,7 +16,7 @@ export const projectList = [
   },
   {
     name: "Camp Nedooae Scouting Map",
-    imgDir: 'images/projects/nedooae/',
+    imgDir: '/portfolio/images/projects/nedooae/',
     thumbnail: 'CampNedooae.png',
     description: "This map was created for a client through NSCC with the goal that it be used by scouts looking to get one of their badges. Data was obtained from the Nova Scotia Topographic Database and processed in ArcGIS Pro. Pro was used to create a basic layout, but all the final edits were completed with Adobe Illustrator.",
     images: ['CampNedooae.png']
